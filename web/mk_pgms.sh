baa15b6e026d7e3780d069157c593eaa1c7b6b34
Lars Vilhuber <lars.vilhuber@cornell.edu>
2013-08-16 09:18:35 -0400

    Added a function in bash

--- web/mk_pgms.sh
+++ web/mk_pgms.sh
@@ -3,15 +3,23 @@ outfile=programs.md
 cat > $outfile << EOF
 High-performance computing for Economists - Programs
 ====================================================
-
-Day 1
------
 EOF
 
-for file in $(find ../programs/day1 -name \*sh -o -name \*R -o -name \*sas| sort)
+function add_day {
+cat >> $outfile << EOF
+
+Day $1
+-----
+EOF
+for file in $(find ../programs/day$1 -name \*sh -o -name \*R -o -name \*sas| sort)
 do
   echo "* [${file##*/}]($file)" >> $outfile
 done
+}
+
+add_day 1
+add_day 2
+add_day 3
 
 multimarkdown $outfile > $(basename $outfile .md).html 
 git add $outfile $(basename $outfile .md).html 
---
+++ web/programs.md
@@ -0,0 +1,12 @@
+High-performance computing for Economists - Programs
+====================================================
+
+Day 1
+-----
+* [1-1-basic-loop.bash](../programs/day1/1-1-basic-loop.bash)
+
+Day 2
+-----
+
+Day 3
+-----
